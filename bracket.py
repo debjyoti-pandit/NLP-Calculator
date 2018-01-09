62d5d3e207b851a639d460e0f12b1ce3e19203b7
DEBJYOTI PANDIT <debjyotipandit35@gmail.com>
2018-01-09 07:12:56 +0530

    Update bracket.py

--- bracket.py
+++ bracket.py
@@ -1,4 +1,5 @@
 def precedence(l):
+##    print(l)
     before = []
     after = []
     count = 0
@@ -7,18 +8,34 @@ def precedence(l):
     shift = 1
     first = 0
     m = []
+    ssq = 0
+    ssc = 0
     for w in l:
+##        print("for: ",w[0],"count",count)
         if w[1] == 'CD':
             if shift and not seen:
                 first = count
             if seen:
-                before.append(first)
-                after.append(count+1)
+                if ssq:
+                    before.append(ssc)
+                    after.append(count+1)
+                    before.append(first)
+                    after.append(count+1)
+                    first = ssc
+                    ssq = 0
+                else:
+                    before.append(first)
+                    after.append(count+1)
+                
             else:
-                seen = 1
+                if w[1] == 'CD':
+                    seen = 1
             distance = 0
         else:
-            if seen:
+            if w[0] == 'square':
+                ssq = 1
+                ssc = count
+            if seen:    
                 if distance > 1:
                     distance = 0
                     seen = 0
@@ -26,8 +43,7 @@ def precedence(l):
                     distance = distance + 1
         count = count + 1
     acc = 0
-    print(before)
-    print(after)
+    before.sort()
     for i in range(len(before)):
         l.insert(before[i]+acc, ('(','('))
         acc += 1
@@ -39,4 +55,8 @@ def precedence(l):
                 q += 1
         l.insert(after[i]+acc+q, (')',')'))
         acc += 1
+##    print("in br: ",l)
     return l 
+
+##ss =[('sum', 'NN'), ('of', 'IN'), ('square', 'JJ'), ('root', 'NN'), ('of', 'IN'), (40.0, 'CD'), ('plus', 'CC'), (60.0, 'CD'), ('and', 'CC'), (21.0, 'CD')]
+##precedence(ss)
